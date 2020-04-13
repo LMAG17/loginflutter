056082dc61e831958fb03d81f9d0e1bf2a1da5c9
LMAG17 <luismiguelalvarez46@gmail.com>
2020-04-13 17:03:21 -0500

    Prueba de libreria graphql

--- cakeboss/lib/main.dart
+++ cakeboss/lib/main.dart
@@ -1,14 +1,28 @@
-import 'package:cakeboss/home.dart';
+import 'package:cakeboss/graph.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Home(),
+    final HttpLink httpLink =
+        HttpLink(uri: "https://countries.trevorblades.com/");
+    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+        link: httpLink,
+        cache: InMemoryCache(),
+      ),
+    );
+    return GraphQLProvider(
+      client: client,
+      child: CacheProvider(
+              child: MaterialApp(
+          title: 'Graph&Flutter',
+          home: GraphPage(),
+        ),
+      ),
     );
   }
 }
